c725aef3411285631b50cc6b0108c934427b741d
ViniciusChanes <vini.chanes@gmail.com>
2023-04-11 17:02:31 -0300

    Adição do controller fornecedor, produtos, junto com cadastro, e o index

---
+++ WebApp_Tarde/Controllers/Fornecedores.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp_Tarde.Controllers
+{
+    public class Fornecedores : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+    }
+}
